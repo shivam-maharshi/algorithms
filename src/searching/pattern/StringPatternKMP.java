2ea13501115943aa4830ac927188695db1dd4f34
shivam-maharshi <shivam.maharshi@gmail.com>
2015-11-05 21:20:32 -0500

    Commit first additions of pattern matching.

---
+++ src/searching/pattern/StringPatternKMP.java
@@ -0,0 +1,10 @@
+package searching.pattern;
+
+public class StringPatternKMP {
+	
+	/*
+	 * Search a string for a pattern using KMP. This is O(n) in worst case.
+	 */
+
+	// TODO:
+}
---
+++ src/searching/pattern/StringPatternSuffixArray.java
@@ -0,0 +1,10 @@
+package searching.pattern;
+
+public class StringPatternSuffixArray {
+
+	/*
+	 * Search a string pattern using a build suffix array which is sorted. Apply
+	 * binary searching on this array to look for the presence of the pattern.
+	 */
+
+}
