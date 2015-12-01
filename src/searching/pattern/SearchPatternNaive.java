a583e81b35bd2ee23ebb1dfad98c6ebf76bd67e8
shivam-maharshi <shivam.maharshi@gmail.com>
2015-12-01 03:06:37 -0500

    Naive String pattern search algorithm.

---
+++ src/searching/pattern/SearchPatternNaive.java
@@ -0,0 +1,40 @@
+package searching.pattern;
+
+/**
+ * Search pattern naively.
+ * 
+ * Link:
+ * http://www.geeksforgeeks.org/searching-for-patterns-set-1-naive-pattern-
+ * searching/
+ * 
+ * @author shivam.maharshi
+ */
+public class SearchPatternNaive {
+
+	public static void match(String s, String p) {
+		int start = 0, i = 0, j = 0;
+		if (s.length() >= p.length()) {
+			while (start <= s.length() - p.length()) {
+				if (s.charAt(i) == p.charAt(j)) {
+					if (j == p.length() - 1) {
+						System.out.println("Pattern at : " + start);
+						j = 0;
+						start++;
+						i = start;
+					}
+					i++;
+					j++;
+				} else {
+					j = 0;
+					start++;
+					i = start;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		SearchPatternNaive.match("abcdefabcdgrabcdgr", "abcdgr");
+	}
+
+}
