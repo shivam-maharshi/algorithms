970f7bf6282904774a5e2ad285816e8f4c693e43
shivam-maharshi <shivam.maharshi@gmail.com>
2015-12-09 12:18:45 -0500

    Added Print All Number Combination.

---
+++ src/math/PrintAllNumberCombination.java
@@ -0,0 +1,30 @@
+package math;
+
+/**
+ * Print all the possible combination of how a number can be broken as sum of 1,
+ * 2 or 3. Ordering is important.
+ * 
+ * Link: http://www.geeksforgeeks.org/write-you-own-power-without-using-multiplication-and-division/
+ * 
+ * @author shivam.maharshi
+ */
+public class PrintAllNumberCombination {
+
+	public static void print(int n, String s) {
+		if (n <= 0) {
+			if (n == 0) {
+				System.out.println(s);
+			}
+			return;
+		} else {
+			print(n - 1, s + "  " + 1);
+			print(n - 2, s + "  " + 2);
+			print(n - 3, s + "  " + 3);
+		}
+	}
+
+	public static void main(String[] args) {
+		PrintAllNumberCombination.print(4, "");
+	}
+
+}
