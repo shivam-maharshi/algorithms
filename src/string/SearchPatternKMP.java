311f98f690684a6105cb583d83818f717d2d19bb
shivam-maharshi <shivam.maharshi@gmail.com>
2016-01-22 03:19:19 -0500

    Added KMP String pattern searching algo.

--- src/string/SearchPatternKMP.java
+++ src/string/SearchPatternKMP.java
@@ -1,5 +1,8 @@
 package string;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Search for pattern in the most efficient way. Worst case O(n) complexity.
  * 
@@ -10,8 +13,59 @@ package string;
  */
 public class SearchPatternKMP {
 
-	public static void match(String s, String p) {
-		
+	public static List<Integer> match(String s, String p) {
+		List<Integer> plist = new ArrayList<>();
+		int[] lps = computeLps(p);
+		int i = 0, j = 0;
+		while (i < s.length()) {
+			if (s.charAt(i) == p.charAt(j)) {
+				i++;
+				j++;
+			}
+			if (j == p.length()) {
+				plist.add(i - j);
+				j = lps[j - 1];
+			} else if (i < s.length() && s.charAt(i) != p.charAt(j)) {
+				if (j != 0)
+					j = lps[j - 1];
+				else
+					i++;
+			}
+		}
+		return plist;
+	}
+
+	private static int[] computeLps(String p) {
+		int[] lps = new int[p.length()];
+		int len = 0;
+		int i = 1;
+		while (i < p.length()) {
+			if (p.charAt(i) == p.charAt(len)) {
+				len++;
+				lps[i] = len;
+				i++;
+			} else {
+				if (len != 0) {
+					/*
+					 * This is super tricky but awesome !! If the current
+					 * character did not match then check if the character lps
+					 * just before it matches. If it does that means that lps
+					 * value +1 is our current lps value, otherwise we have got
+					 * to check with the sequence previous to that.
+					 */
+					len = lps[len - 1];
+				} else {
+					i++;
+				}
+			}
+		}
+		return lps;
+	}
+
+	public static void main(String[] args) {
+		List<Integer> pl = match("ABABDABACDABABCABABABABCABAB", "ABABCABAB");
+		for (int p : pl)
+			System.out.print(p + " ");
 	}
 
 }
