fcac4689c75c4cbeedca2ac4deb4f299d55f9267
shivam-maharshi <shivam.maharshi@gmail.com>
2016-01-24 17:09:07 -0500

    Added Detect Cycle In an Undirected Graph using Union Algo.

---
+++ src/graph/DetectCycleInUndirectedGraphUsingUnion.java
@@ -0,0 +1,40 @@
+package graph;
+
+import data.structures.GraphAdjList;
+import data.structures.Union;
+
+/**
+ * Detect cycle in an undirected graph via Union Find algorithm using Disjoint
+ * Data Set.
+ * 
+ * Link: http://www.geeksforgeeks.org/union-find/
+ * 
+ * @author shivam.maharshi
+ */
+public class DetectCycleInUndirectedGraphUsingUnion {
+
+	public static boolean isCyclic(GraphAdjList graph) {
+		Union u = new Union(graph.size());
+		for (int i = 0; i < graph.size(); i++) {
+			for (int j = 0; j < graph.getAdjList(i).size(); j++) {
+				if (graph.getAdjList(i).get(j) > i) {
+					/*
+					 * This condition is required because of the non directed
+					 * graph implied transitive property. A->B = B->A
+					 */
+					if (!u.isConnected(i, graph.getAdjList(i).get(j)))
+						u.connect(i, graph.getAdjList(i).get(j));
+					else
+						return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(isCyclic(GraphAdjList.getPopulatedCyclicUndirectedGraph()));
+		System.out.println(isCyclic(GraphAdjList.getPopulatedNonCyclicUndirectedGraph()));
+	}
+
+}
