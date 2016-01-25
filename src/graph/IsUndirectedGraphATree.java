40a419336a34ef19a6e2d4b05f47d1afdde692e2
shivam-maharshi <shivam.maharshi@gmail.com>
2016-01-24 20:58:54 -0500

    Added is Undirected Graph a Tree.

---
+++ src/graph/IsUndirectedGraphATree.java
@@ -0,0 +1,24 @@
+package graph;
+
+import data.structures.GraphAdjList;
+
+/**
+ * Check if the undirected graph is a tree. An undirected graph is tree if it
+ * has following properties. 1) There is no cycle. 2) The graph is connected.
+ * 
+ * Link: http://geeksquiz.com/check-given-graph-tree/
+ * 
+ * @author shivam.maharshi
+ */
+public class IsUndirectedGraphATree {
+
+	public static boolean isTree(GraphAdjList graph) {
+		return IsUndirectedGraphConnected.isConnected(graph) && !DetectCycleInUndirectedGraph.isCyclic(graph);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(isTree(GraphAdjList.getPopulatedCyclicUndirectedGraph()));
+		System.out.println(isTree(GraphAdjList.getPopulatedNonCyclicUndirectedGraph()));
+	}
+
+}
