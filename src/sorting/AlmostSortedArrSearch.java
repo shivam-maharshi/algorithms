2c103929555cac4e7fb410279c616da24b784ffe
shivam-maharshi <shivam.maharshi@gmail.com>
2016-02-01 23:29:31 -0500

    Added search in an almost sorted array.

---
+++ src/sorting/AlmostSortedArrSearch.java
@@ -0,0 +1,16 @@
+package sorting;
+
+/**
+ * Given an array which is sorted, but after sorting some elements are moved to
+ * either of the adjacent positions, i.e., arr[i] may be present at arr[i+1] or
+ * arr[i-1].
+ * 
+ * Link: http://www.geeksforgeeks.org/search-almost-sorted-array/
+ *
+ * @author shivam.maharshi
+ */
+public class AlmostSortedArrSearch {
+	
+	// TODO:
+
+}
