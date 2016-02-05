c7cdd078c8d9ce6c46cbab21b0dfa8511af07798
shivam-maharshi <shivam.maharshi@gmail.com>
2016-02-04 22:11:49 -0500

    Added longest common substring.

---
+++ src/string/LongestCommonSubstring.java
@@ -0,0 +1,16 @@
+package string;
+
+/**
+ * Find the longest string (or strings) that is a substring (or are substrings)
+ * of two or more strings.
+ * 
+ * Link:
+ * https://en.wikipedia.org/wiki/Longest_common_substring_problem#Suffix_tree
+ * 
+ * @author shivam.maharshi
+ */
+public class LongestCommonSubstring {
+	
+	// TODO:
+
+}
