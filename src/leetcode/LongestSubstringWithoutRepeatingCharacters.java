5727e53358dce157d0cfb8033eedab39f88ced97
shivam-maharshi <shivam.maharshi@gmail.com>
2016-07-09 19:56:08 -0400

    Added LongestSubStringWithoutRepitions

---
+++ src/leetcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+/**
+ * Link: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ * 
+ * @author shivam.maharshi
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+
+	int[] cc = new int[256]; 
+	
+    public int lengthOfLongestSubstring(String s) {
+    	if(s==null || s.isEmpty())
+    		return 0;
+    	int i=0,j=0, len=0;
+    	while(j<s.length()) {
+    		cc[s.charAt(j)]++;
+    		while(hasRepitions()) {
+    			cc[s.charAt(i)]--;
+    			i++;
+    		}
+    		if(j-i +1 > len)
+				len = j-i+1;
+    		j++;
+    	}
+    	return len;
+    }
+    
+    public boolean hasRepitions() {
+    	for(int i=0;i<256;i++) {
+    		if(cc[i]>1)
+    			return true;
+    	}
+    	return false;
+    }
+	
+}
