402eeee252bbd3a9f21f06b901e421441c7e129f
shivam-maharshi <shivam.maharshi@gmail.com>
2016-09-03 17:50:23 -0400

    Added generate parenthesis.

--- src/leetcode/AddTwoNumbers.java
+++ src/leetcode/AddTwoNumbers.java
@@ -5,15 +5,6 @@ package leetcode;
  * 
  * @author shivam.maharshi
  */
-class ListNode {
-	int val;
-	ListNode next;
-
-	ListNode(int x) {
-		val = x;
-	}
-}
-
 public class AddTwoNumbers {
 
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
