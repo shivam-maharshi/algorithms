46e88e274a51d84f80491e1b5d97490269f7abf9
shivam-maharshi <shivam.maharshi@gmail.com>
2016-09-03 18:18:50 -0400

    Added swap nodes in pairs.

---
+++ src/leetcode/SwapNodesInPairs.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+/**
+ * Leetcode: https://leetcode.com/problems/swap-nodes-in-pairs/
+ * 
+ * @author shivam.maharshi
+ */
+public class SwapNodesInPairs {
+
+  public ListNode swapPairs(ListNode head) {
+    if (head == null || head.next == null)
+      return head;
+    head = swap(head, head.next);
+    ListNode start = head.next;
+    while (start != null && start.next != null) {
+      ListNode temp = swap(start.next, start.next.next);
+      start.next = temp;
+      start = temp.next;
+    }
+    return head;
+  }
+
+  public ListNode swap(ListNode f, ListNode s) {
+    if (f != null && s == null)
+      return f;
+    else {
+      ListNode temp = s.next;
+      s.next = f;
+      f.next = temp;
+      return s;
+    }
+  }
+
+}
