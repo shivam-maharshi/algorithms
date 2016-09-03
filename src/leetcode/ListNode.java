2184dc404936dcba0fcbc7cee9ba4ad396bd575b
shivam-maharshi <shivam.maharshi@gmail.com>
2016-09-03 17:32:17 -0400

    Added Merge sorted list.

---
+++ src/leetcode/ListNode.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode(int x) {
+    val = x;
+  }
+}
---
+++ src/leetcode/MergeSortedLinkedList.java
@@ -0,0 +1,51 @@
+package leetcode;
+
+/**
+ * Leetcode: https://leetcode.com/problems/merge-two-sorted-lists/
+ * 
+ * @author shivam.maharshi
+ */
+public class MergeSortedLinkedList {
+
+  public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    if (l1 == null && l2 == null)
+      return null;
+    else if (l1 == null)
+      return l2;
+    else if (l2 == null)
+      return l1;
+    else {
+      ListNode head = null;
+      if (l1.val < l2.val) {
+        head = l1;
+        l1 = l1.next;
+      } else {
+        head = l2;
+        l2 = l2.next;
+      }
+      ListNode temp = head;
+      while (l1 != null && l2 != null) {
+        if (l1.val < l2.val) {
+          temp.next = l1;
+          l1 = l1.next;
+        } else {
+          temp.next = l2;
+          l2=l2.next;
+        }
+        temp = temp.next;
+      }
+      while(l1!=null) {
+        temp.next = l1;
+        l1 = l1.next;
+        temp = temp.next;
+      }
+      while(l2!=null) {
+        temp.next = l2;
+        l2 = l2.next;
+        temp = temp.next;
+      }
+      return head;
+    }
+  }
+
+}
