dddd90fca1856febaa8213dfb2c7c3aa02d73d1c
shivam-maharshi <shivam.maharshi@gmail.com>
2016-10-03 17:00:51 -0400

    Added Longest Increasing Subsequence.

--- src/dp/LongestIncreasingSubsequence.java
+++ src/dp/LongestIncreasingSubsequence.java
@@ -16,52 +16,50 @@ import java.util.Stack;
  */
 public class LongestIncreasingSubsequence {
 
-	public static int get(int[] a) {
-		Stack<Integer> stack = new Stack<Integer>();
-		stack.push(a[0]);
-		int withCur = get(a, 1, stack) + 1;
-		stack.pop();
-		int withoutCur = get(a, 1, stack);
-		return Math.max(withoutCur, withCur);
-	}
+  public static int get(int[] a) {
+    Stack<Integer> stack = new Stack<Integer>();
+    stack.push(a[0]);
+    int withCur = get(a, 1, stack) + 1;
+    stack.pop();
+    int withoutCur = get(a, 1, stack);
+    return Math.max(withoutCur, withCur);
+  }
 
-	private static int get(int[] a, int index, Stack<Integer> stack) {
-		if (index == a.length)
-			return 0;
-		int withCur = 0;
-		if (!stack.isEmpty() && a[index] > stack.peek()) {
-			stack.push(a[index]);
-			withCur = get(a, index + 1, stack) + 1;
-			stack.pop();
-		}
-		int withoutCur = get(a, index + 1, stack);
-		return Math.max(withoutCur, withCur);
-	}
+  private static int get(int[] a, int index, Stack<Integer> stack) {
+    if (index == a.length)
+      return 0;
+    int withCur = 0;
+    if (!stack.isEmpty() && a[index] > stack.peek()) {
+      stack.push(a[index]);
+      withCur = get(a, index + 1, stack) + 1;
+      stack.pop();
+    }
+    int withoutCur = get(a, index + 1, stack);
+    return Math.max(withoutCur, withCur);
+  }
 
-	public static void main(String[] args) {
-		int[] a = { 10, 22, 9, 33, 21, 50, 41, 60, 80 };
-		System.out.println(get(a));
-		System.out.println(getMax(a));
-	}
-	
-	public static int getMax(int[] a) {
-	  int[] dp = new int[a.length];
-	  for(int i=0; i<a.length; i++) {
-	    int count = 0;
-	    for (int j=i-1; j>=0; j--) {
-	      if (a[j] < a[i]) {
-	        if (count < (dp[j] + 1)) {
-	          count = dp[j] + 1;
-	        }
-	      } else {
-	        if (count < dp[j]) {
-            count = dp[j];
-          }
-	      }
-	    }
-	    dp[i] = count == 0 ? 1 : count;
-	  }
-	  return dp[a.length-1];
-	}
+  public static void main(String[] args) {
+    int[] a = { 10, 9, 2, 5, 3, 7, 101, 18 };
+    System.out.println(getMax(a));
+  }
+
+  public static int getMax(int[] a) {
+    if (a.length == 0)
+      return 0;
+    int[] dp = new int[a.length];
+    dp[0] = 1;
+    for (int i = 1; i < a.length; i++) {
+      for (int j = i - 1; j >= 0; j--) {
+        if (a[i] > a[j]) {
+          dp[i] = Math.max(dp[i], dp[j] + 1);
+        }
+      }
+      dp[i] = (dp[i] == 0) ? 1 : dp[i];
+    }
+    int max = 0;
+    for (int i = 0; i < a.length; i++)
+      max = Math.max(dp[i], max);
+    return max;
+  }
 
 }
