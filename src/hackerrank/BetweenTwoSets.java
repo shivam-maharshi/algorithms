89fde50d649d06356794f321ca3e2e6dff21f164
shivam-maharshi <shivam.maharshi@gmail.com>
2016-11-02 15:23:21 -0400

    Added Between Two Sets.

---
+++ src/hackerrank/BetweenTwoSets.java
@@ -0,0 +1,47 @@
+package hackerrank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+/**
+ * @author shivam.maharshi
+ */
+public class BetweenTwoSets extends TestCase {
+  
+  @Test
+  public static void test() {
+    int[] a = new int[] {2, 4};
+    int[] b = new int[] {16, 32, 96};
+    assertEquals(3, get(a, b));
+  }
+
+  public static int get(int[] a, int[] b) {
+    List<Integer> n = new ArrayList<>();
+    for (int i = 1; i <= 100; i++) {
+      for (int j = 0; j < a.length; j++) {
+        if (i % a[j] == 0 && j == a.length - 1) {
+          n.add(i);
+        } else if (i % a[j] != 0) {
+          break;
+        }
+      }
+    }
+
+    for (int i = 0; i < n.size(); i++) {
+      for (int j = 0; j < b.length; j++) {
+        if ((b[j] % n.get(i)) != 0) {
+          n.remove(i);
+          i--;
+          break;
+        }
+      }
+    }
+
+    return n.size();
+  }
+
+}
