27d51570df2d93242c8448aa8630bfae4a32d1d3
shivam-maharshi <shivam.maharshi@gmail.com>
2016-11-12 15:44:23 -0500

    Added Tree Inorder Traversal.

---
+++ src/leetcode/medium/BinaryTreeInorderTraversal.java
@@ -0,0 +1,34 @@
+package leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Link: https://leetcode.com/problems/binary-tree-inorder-traversal/
+ * 
+ * @author shivam.maharshi
+ */
+public class BinaryTreeInorderTraversal {
+
+  public static List<Integer> inorderTraversal(TreeNode root) {
+    List<Integer> l = new ArrayList<>();;
+    if (root != null)
+      parse(root, l);
+    return l;
+  }
+
+  public static void parse(TreeNode root, List<Integer> l) {
+    if (root.left != null)
+      parse(root.left, l);
+    l.add(root.val);
+    if (root.right != null)
+      parse(root.right, l);
+  }
+
+  private static class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+  }
+
+}
