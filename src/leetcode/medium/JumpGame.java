29e89dc4b64cc9a09b7ff1ca3842eeb587d2c612
shivam-maharshi <shivam.maharshi@gmail.com>
2016-11-11 21:43:30 -0500

    Added Jump Game.

---
+++ src/leetcode/medium/JumpGame.java
@@ -0,0 +1,33 @@
+package leetcode.medium;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+/**
+ * Link: https://leetcode.com/problems/jump-game/
+ * 
+ * @author shivam.maharshi
+ */
+public class JumpGame extends TestCase {
+
+  @Test
+  public static void test() {
+    assertEquals(true, canJump(new int[]{2}));
+    assertEquals(true, canJump(new int[]{2,3}));
+    assertEquals(false, canJump(new int[]{0,1}));
+    assertEquals(true, canJump(new int[]{2,3,1,1,4}));
+    assertEquals(false, canJump(new int[]{3,2,1,0,4}));
+  }
+
+  public static boolean canJump(int[] nums) {
+    if (nums == null || nums.length == 0 || nums.length == 1)
+      return true;
+    int last = nums.length - 1;
+    for (int i = nums.length - 2; i >= 0; i--)
+      if (i + nums[i] >= last)
+        last = i;
+    return last == 0;
+  }
+
+}
