7eec60233a8a0da47fd207d81e60b811e3fb1d19
shivam-maharshi <shivam.maharshi@gmail.com>
2016-11-13 15:18:51 -0500

    Added Missing Number

---
+++ src/leetcode/medium/MissingNumber.java
@@ -0,0 +1,28 @@
+package leetcode.medium;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+/**
+ * Link: https://leetcode.com/problems/missing-number/
+ * 
+ * @author shivam.maharshi
+ */
+public class MissingNumber extends TestCase {
+  
+  @Test
+  public static void test() {
+    assertEquals(2, missingNumber(new int[]{0,1,3}));
+    assertEquals(0, missingNumber(new int[]{1,2,3}));
+    assertEquals(7, missingNumber(new int[]{0,1,2,3,4,5,6,8,9}));
+  }
+
+  public static int missingNumber(int[] nums) {
+    int r = 0;
+    for(int i=0; i<nums.length; i++)
+      r ^= nums[i] ^ i;
+    return r ^ nums.length;
+  }
+
+}
