c4ec8d0d3e7ae64f86e50662f932543b28b083e9
shivam-maharshi <shivam.maharshi@gmail.com>
2016-12-22 22:19:16 -0500

    Added Verify Preorder Serialization Of a Binary Tree.

---
+++ src/main/java/leetcode/medium/VerifyPreorderSerializationOfABinaryTree.java
@@ -0,0 +1,47 @@
+package leetcode.medium;
+
+import java.util.Stack;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+/**
+ * Link:
+ * https://leetcode.com/problems/verify-preorder-serialization-of-a-binary-tree/
+ * 
+ * @author shivam.maharshi
+ */
+public class VerifyPreorderSerializationOfABinaryTree extends TestCase {
+
+  @Test
+  public static void test() {
+    assertEquals(true, isValidSerialization("#"));
+    assertEquals(true, isValidSerialization("9,#,#"));
+    assertEquals(false, isValidSerialization("#,#"));
+    assertEquals(false, isValidSerialization("#,1"));
+    assertEquals(false, isValidSerialization("1,#"));
+    assertEquals(false, isValidSerialization("9,#,#,1"));
+    assertEquals(true, isValidSerialization("9,#,92,#,#"));
+    assertEquals(true, isValidSerialization("9,3,4,#,#,1,#,#,2,#,6,#,#"));
+  }
+
+  public static boolean isValidSerialization(String preorder) {
+    if (preorder == null || preorder.isEmpty())
+      return false;
+    String[] ss = preorder.split(",");
+    Stack<String> s = new Stack<>();
+    for (int i = 0; i < ss.length; i++) {
+      s.push(ss[i]);
+      while (s.size() >= 3 && s.get(s.size() - 1).equals("#") && s.get(s.size() - 2).equals("#")
+          && !s.get(s.size() - 3).equals("#")) {
+        s.pop();
+        s.pop();
+        s.pop();
+        s.push("#");
+      }
+    }
+    return s.size() == 1 && s.pop().equals("#");
+  }
+
+}
