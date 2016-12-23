391877615c3ca20f9640258c0d65aa2e6cd9c235
shivam-maharshi <shivam.maharshi@gmail.com>
2016-12-23 15:04:12 -0500

    Added Wiggle Subsequence

---
+++ src/main/java/leetcode/medium/LongestAbsoluteFilePath.java
@@ -0,0 +1,17 @@
+package leetcode.medium;
+
+/**
+ * Link: https://leetcode.com/problems/longest-absolute-file-path/
+ * 
+ * @author shivam.maharshi
+ */
+public class LongestAbsoluteFilePath {
+
+  public static int lengthLongestPath(String input) {
+    if (input == null || input.isEmpty() || !input.contains("."))
+      return 0;
+    int n = 0;
+    return n;
+  }
+
+}
