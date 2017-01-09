87d3a73714a56ac3f75577f3faacfa8959b7c9b8
shivam-maharshi <shivam.maharshi@gmail.com>
2017-01-08 20:41:34 -0500

    Added Populating Next Right Pointers In Each Node II.

---
+++ src/main/java/leetcode/hard/PopulatingNextRightPointersInEachNodeII.java
@@ -0,0 +1,43 @@
+package leetcode.hard;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Link: https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/ 
+ * 
+ * @author shivam.maharshi
+ */
+public class PopulatingNextRightPointersInEachNodeII {
+
+  public void connect(TreeLinkNode root) {
+    if (root == null)
+      return;
+    Queue<TreeLinkNode> q = new LinkedList<>();
+    root.next = null;
+    q.add(root);
+    q.add(null);
+    while (!q.isEmpty()) {
+      TreeLinkNode n = q.poll();
+      if (n != null) {
+        n.next = q.peek();
+        if (n.left != null)
+          q.add(n.left);
+        if (n.right != null)
+          q.add(n.right);
+        if (q.peek() == null)
+          q.add(null);
+      }
+    }
+  }
+
+  class TreeLinkNode {
+    int val;
+    TreeLinkNode left, right, next;
+
+    TreeLinkNode(int x) {
+      val = x;
+    }
+  }
+
+}
