c0fa3c95efa329babd6cfb7d28c521e84a464c88
shivam-maharshi <shivam.maharshi@gmail.com>
2017-02-01 02:18:20 -0500

    Added Cutting Rod.

---
+++ src/main/java/dp/CuttingRod.java
@@ -0,0 +1,28 @@
+package dp;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+/**
+ * Link: http://www.geeksforgeeks.org/dynamic-programming-set-13-cutting-a-rod/
+ * 
+ * @author shivam.maharshi
+ */
+public class CuttingRod extends TestCase {
+
+  @Test
+  public static void test() {
+    assertEquals(22, max(new int[] { 1, 5, 8, 9, 10, 17, 17, 20 }, 8));
+  }
+
+  public static int max(int[] p, int n) {
+    if (n <= 0)
+      return 0;
+    int[] dp = p.clone();
+    for (int i = 1; i < n; i++)
+      for (int j = 0; j < i; j++)
+        dp[i] = Math.max(dp[i], dp[i - j - 1] + dp[j]);
+    return dp[n - 1];
+  }
+}
