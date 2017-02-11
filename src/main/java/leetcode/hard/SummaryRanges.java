a1c9e28cbf06f014fe8e07b1a98d7406c706b59b
shivam-maharshi <shivam.maharshi@gmail.com>
2017-02-10 21:47:11 -0500

    Added Summary Ranges.

--- src/main/java/leetcode/hard/SummaryRanges.java
+++ src/main/java/leetcode/hard/SummaryRanges.java
@@ -2,6 +2,7 @@ package leetcode.hard;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeMap;
 
 /**
  * Link: https://leetcode.com/problems/data-stream-as-disjoint-intervals/
@@ -21,70 +22,48 @@ public class SummaryRanges {
     s.addNum(2);
     s.print();
     s.addNum(6);
+    s.print();
   }
 
-  List<Interval> list;
+  TreeMap<Integer, Interval> map;
 
   public SummaryRanges() {
-    this.list = new ArrayList<>();
+    this.map = new TreeMap<Integer, Interval>();
   }
 
   public void addNum(int val) {
-    if (list.isEmpty()) {
-      list.add(new Interval(val, val));
-      return;
-    }
-
-    int i = search(val);
-    if (val == (list.get(i).start - 1)) {
-      if (i > 0 && val == list.get(i - 1).end + 1) {
-        Interval in = new Interval(list.get(i - 1).start, list.get(i).end);
-        list.remove(i);
-        list.set(i - 1, in);
+    if (!map.containsKey(val)) {
+      Integer l = map.lowerKey(val), h = map.higherKey(val);
+      if (l != null && h != null && map.get(l).end == val - 1 && h == val + 1) {
+        map.put(l, new Interval(l, map.get(h).end));
+        map.remove(h);
+      } else if (l != null && map.get(l).end >= val - 1) {
+        map.put(l, new Interval(l, Math.max(map.get(l).end, val)));
+      } else if (h != null && h == val + 1) {
+        map.put(val, new Interval(val, map.get(h).end));
+        map.remove(h);
       } else {
-        list.set(i, new Interval(val, list.get(i).end));
+        map.put(val, new Interval(val, val));  
       }
-    } else if (i > 0 && val == list.get(i - 1).end + 1) {
-      list.set(i - 1, new Interval(list.get(i - 1).start, val));
-    } else {
-      list.add(0, new Interval(val, val));
     }
   }
 
   public List<Interval> getIntervals() {
-    return list;
-  }
-
-  private int search(int n) {
-    int l = 0, h = this.list.size() - 1, m = 0;
-    while (l <= h) {
-      m = l + (h - l) / 2;
-      if (n == list.get(m).start)
-        return m;
-      else if (n < list.get(m).start) {
-        h = m - 1;
-      } else {
-        l = m + 1;
-      }
-    }
-    return m;
+    List<Interval> l = new ArrayList<>();
+    for (Interval i : map.values())
+      l.add(i);
+    return l;
   }
 
   public void print() {
-    for (Interval it : list)
+    for (Interval it : map.values())
       System.out.print("[" + it.start + " - " + it.end + "]");
-
     System.out.println();
   }
 
   private class Interval {
     int start;
     int end;
-
-    Interval() {
-      start = 0;
-      end = 0;
-    }
 
     Interval(int s, int e) {
       start = s;
