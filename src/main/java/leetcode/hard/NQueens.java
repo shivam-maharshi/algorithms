cd14c9f2d256386a8e6ea7a7845e275bc319713e
shivam-maharshi <shivam.maharshi@gmail.com>
2017-02-10 21:05:28 -0500

    Added NQueens Problem.

--- src/main/java/leetcode/hard/NQueens.java
+++ src/main/java/leetcode/hard/NQueens.java
@@ -1,38 +1,40 @@
 package leetcode.hard;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 import junit.framework.TestCase;
 
 /**
- * Link: https://leetcode.com/problems/n-queens-ii/
+ * Link: https://leetcode.com/problems/n-queens/
  * 
  * @author shivam.maharshi
  */
-public class NQueensII extends TestCase {
+public class NQueens extends TestCase {
 
   @Test
   public static void test() {
-    assertEquals(0, totalNQueens(0));
-    assertEquals(1, totalNQueens(1));
-    assertEquals(0, totalNQueens(2));
-    assertEquals(0, totalNQueens(3));
-    assertEquals(2, totalNQueens(4));
-    assertEquals(10, totalNQueens(5));
-    assertEquals(92, totalNQueens(8));
-    assertEquals(724, totalNQueens(10));
+    assertEquals(1, solveNQueens(1).size());
+    assertEquals(0, solveNQueens(2).size());
+    assertEquals(0, solveNQueens(3).size());
+    assertEquals(2, solveNQueens(4).size());
+    assertEquals(10, solveNQueens(5).size());
+    assertEquals(92, solveNQueens(8).size());
+    assertEquals(724, solveNQueens(10).size());
   }
 
-  public static int totalNQueens(int n) {
-    int[] r = new int[1];
+  public static List<List<String>> solveNQueens(int n) {
+    List<List<String>> r = new ArrayList<List<String>>();
     boolean[][] m = new boolean[n][n];
     get(m, 0, r);
-    return r[0];
+    return r;
   }
 
-  public static void get(boolean[][] a, int i, int[] r) {
+  public static void get(boolean[][] a, int i, List<List<String>> r) {
     if (i == a.length) {
-      r[0]++;
+      add(a, r);
       return;
     }
     for (int j = 0; j < a.length; j++) {
@@ -42,6 +44,17 @@ public class NQueensII extends TestCase {
         a[i][j] = false;
       }
     }
+  }
+
+  public static void add(boolean[][] a, List<List<String>> l) {
+    List<String> ll = new ArrayList<String>();
+    for (int i = 0; i < a.length; i++) {
+      String s = "";
+      for (int j = 0; j < a.length; j++)
+        s += a[i][j] ? "Q" : ".";
+      ll.add(s);
+    }
+    l.add(ll);
   }
 
   public static boolean isValidMove(boolean[][] a, int row, int col) {
