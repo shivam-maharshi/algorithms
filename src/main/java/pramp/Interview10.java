eaeb8e7c348d9f16a4b0147da8994493013589dc
shivam-maharshi <shivam.maharshi@gmail.com>
2017-04-16 23:55:07 -0400

    Updated description and gradle file.

--- src/main/java/pramp/Interview10.java
+++ src/main/java/pramp/Interview10.java
@@ -1,34 +1,43 @@
-class Pramp {
+package pramp;
 
-   public static String get(String s, char[] a) {
-      if (a == null || a.length == 0 || s == null || s.length() == 0 || a.length > s.legnth())
-         return null;
-      String r = null;
-      int[] c = new int[256];
-      for (int i=0; i< a.length; i++)
-         c[s.charAt(i)]++;
-      int i = 0, j = a.length - 1;
-      while (i <= s.length() - a.length && j <= s.length()) {
-         if (allPresent(a, c)) {
-            r = (r == null || r.length() > j - i) ? s.substring(i, j) : r; // 5, 8
-            if (r.length() == a.length)
-               return r;
-            c[s.charAt(i)]--;
-            i++;
-         } else {
-            j++;
-            c[s.charAt(j)]++;
-         }
+/**
+ * Given a string and a character set, return the shortest substring that
+ * contains all the characters.
+ *
+ * @author: shivam.maharshi
+ */
+
+public class Interview10 {
+
+  public static String get(String s, char[] a) {
+    if (a == null || a.length == 0 || s == null || s.length() == 0 || a.length > s.length())
+      return null;
+    String r = null;
+    int[] c = new int[256];
+    for (int i = 0; i < a.length; i++)
+      c[s.charAt(i)]++;
+    int i = 0, j = a.length - 1;
+    while (i <= s.length() - a.length && j <= s.length()) {
+      if (allPresent(a, c)) {
+        r = (r == null || r.length() > j - i) ? s.substring(i, j) : r; // 5, 8
+        if (r.length() == a.length)
+          return r;
+        c[s.charAt(i)]--;
+        i++;
+      } else {
+        j++;
+        c[s.charAt(j)]++;
       }
-      return r;
-   }
-   
-   public static boolean allPresent(char[] a, int[] c) {
-      for (char aa : a) {
-         if (c[aa] == 0)
-            return false;
-         c[aa]--;
-      }
-      return true;
-   }
+    }
+    return r;
+  }
+
+  public static boolean allPresent(char[] a, int[] c) {
+    for (char aa : a) {
+      if (c[aa] == 0)
+        return false;
+      c[aa]--;
+    }
+    return true;
+  }
 }
--- src/main/java/pramp/Interview11.java
+++ src/main/java/pramp/Interview11.java
@@ -1,43 +1,49 @@
-class Pramp {
-   public static void main(String[] args) {
-      String pramp = "Practice Makes Perfect";
-      System.out.println(pramp);
-   }
-   
-   
-   public static void print(int[][] a) {
-      if (a == null || a.length == 0 || a[0].length == 0)
-         return;
-      int x = 0,  y = 0, r = a.length, c = a[0].length;  // r = 4, c = 5
-      
-      while (r > 0 && c > 0) {                         
-        
-         for (int j = x; j < x + c - 1; j++) { // 0 + 5 - 1 < 4
-            System.out.print(a[y][x] + " ");  // 1, 2, 3, 4 -> x = 3
-            x++; // 4
-         }
-         
-         for (int i = y; i < y + r - 1; i++) { // i < 0 + 4 - 1 < 3
-            System.out.print(a[y][x] + " "); // 5, 10, 15
-            y++;
-         } // y = 3
-         
-         for (int j = x; j > x - c + 1; j--) { // j > 4 - 5 + 1 > 0, j = 4
-            System.out.print(a[y][x] + " "); // 20, 19, 18, 17
-            x--;
-         } // x = 0
-         
-         r--;
-         // 3
-         for (int i = y; i > y - r + 1; i--) { // i = 3, i > 3 - 3 + 1 > 1
-            System.out.print(a[y][x] + " "); // 16, 11
-            y--;
-         } //
-         
-         r--; // 2
-         c--; // 4
+package pramp;
+
+/**
+ * Traverse a square matrix in a spiral fashion.
+ * 
+ * @author shivam.maharshi
+ */
+public class Interview11 {
+  public static void main(String[] args) {
+    String pramp = "Practice Makes Perfect";
+    System.out.println(pramp);
+  }
+
+  public static void print(int[][] a) {
+    if (a == null || a.length == 0 || a[0].length == 0)
+      return;
+    int x = 0, y = 0, r = a.length, c = a[0].length; // r = 4, c = 5
+
+    while (r > 0 && c > 0) {
+
+      for (int j = x; j < x + c - 1; j++) { // 0 + 5 - 1 < 4
+        System.out.print(a[y][x] + " "); // 1, 2, 3, 4 -> x = 3
+        x++; // 4
       }
-      
-   }
-      
+
+      for (int i = y; i < y + r - 1; i++) { // i < 0 + 4 - 1 < 3
+        System.out.print(a[y][x] + " "); // 5, 10, 15
+        y++;
+      } // y = 3
+
+      for (int j = x; j > x - c + 1; j--) { // j > 4 - 5 + 1 > 0, j = 4
+        System.out.print(a[y][x] + " "); // 20, 19, 18, 17
+        x--;
+      } // x = 0
+
+      r--;
+      // 3
+      for (int i = y; i > y - r + 1; i--) { // i = 3, i > 3 - 3 + 1 > 1
+        System.out.print(a[y][x] + " "); // 16, 11
+        y--;
+      } //
+
+      r--; // 2
+      c--; // 4
+    }
+
+  }
+
 }
